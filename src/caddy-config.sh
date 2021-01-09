38dd24a5c6442068cfc0e3beb8eec2a0903840c0
rootmelo92118 <rootmelo92118@gmail.com>
2021-01-09 20:26:32 +0800

    first commit

---
+++ src/caddy-config.sh
@@ -0,0 +1,64 @@
+# local email=$(((RANDOM << 22)))
+# tls ${email}@gmail.com
+case $v2ray_transport in
+4|33)
+	if [[ $is_path ]]; then
+		cat >/etc/caddy/Caddyfile <<-EOF
+$domain {
+    gzip
+	timeouts none
+    proxy / $proxy_site {
+        except /${path}
+    }
+    proxy /${path} 127.0.0.1:${v2ray_port} {
+        without /${path}
+        websocket
+    }
+}
+import sites/*
+		EOF
+	else
+		cat >/etc/caddy/Caddyfile <<-EOF
+$domain {
+	timeouts none
+	proxy / 127.0.0.1:${v2ray_port} {
+		websocket
+	}
+}
+import sites/*
+		EOF
+	fi
+	;;
+5)
+	if [[ $is_path ]]; then
+		cat >/etc/caddy/Caddyfile <<-EOF
+$domain {
+    gzip
+	timeouts none
+    proxy / $proxy_site {
+        except /${path}
+    }
+    proxy /${path} https://127.0.0.1:${v2ray_port} {
+        header_upstream Host {host}
+		header_upstream X-Forwarded-Proto {scheme}
+		insecure_skip_verify
+    }
+}
+import sites/*
+		EOF
+	else
+		cat >/etc/caddy/Caddyfile <<-EOF
+$domain {
+	timeouts none
+	proxy / https://127.0.0.1:${v2ray_port} {
+        header_upstream Host {host}
+		header_upstream X-Forwarded-Proto {scheme}
+		insecure_skip_verify
+	}
+}
+import sites/*
+		EOF
+	fi
+	;;
+
+esac
---
+++ src/config.sh
@@ -0,0 +1,20 @@
+
+# vmess
+_load vmess-config.sh
+# ss
+_load ss-config.sh
+
+# socks
+_load socks-config.sh
+
+# mtproto
+_load mtproto-config.sh
+
+# ban bt
+_load ban_bt.sh
+
+# ban ad
+_load ban_ad.sh
+
+# custom config
+_load custom_config.sh
